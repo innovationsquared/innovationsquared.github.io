0fbd03e1d9a3de8d66482911cd601aa790d48db8
innovationsquared <pettiscd@appstate.edu>
2025-05-23 12:43:06 -0500

    blinking cursor

--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -1,11 +1,29 @@
 var i = 0;
 var txt = "Hi! I'm Calvin.";
 var speed = 250;
+var cursorVisible = false;
 
 function type() {
   if (i < txt.length) {
-    document.getElementById("typewriter").innerHTML += txt.charAt(i);
+document.getElementById("typewriter").innerHTML += txt.charAt(i);
     i++;
+    setTimeout(type, speed);
+  } else {
+    flashCursor();
+  }
+
+  
+}
+
+function flashCursor() {
+  var text = document.getElementById("typewriter");
+  if (!cursorVisible) {
+    text.innerHTML += "&#x258A";
+    cursorVisible = true;
+    setTimeout(type, speed);
+  } else {
+    text.innerHTML = text.innerHTML.slice(0,-1);
+    cursorVisible = false;
     setTimeout(type, speed);
   }
   
