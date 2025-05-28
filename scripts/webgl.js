3dce5dc9bfc083bb5398fffd5db1e4616f134e4e
innovationsquared <pettiscd@appstate.edu>
2025-05-28 19:47:54 -0400

    triangle

--- scripts/webgl.js
+++ scripts/webgl.js
@@ -1,24 +1,78 @@
-const canvas = document.getElementById("webGLCanvas");
+function main() {
+    const canvas = document.getElementById("webGLCanvas");
 const gl = canvas.getContext("webgl");
 
 if (!gl) {
     alert("WebGL not enabled!");
 }
 
-gl.clearColor(1, 0, 0, 1);
-
-gl.clear(gl.COLOR_BUFFER_BIT);
 
 //vertex shader
 const vShader =  `
-attribute vec4 aVertexPosition;
-uniform mat4 uModelViewMatrix;
-uniform mat4 uProjectionMatrix;
+attribute vec2 aVertexPosition;
 void main() {
-    gl_position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+   gl_Position = vec4(aVertexPosition, 0.0, 1.0);
 }`;
 
 const fShader = `
 void main() {
-    gl_fragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }`;
+
+var compileShader = function(source, type, typeString) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert("ERROR IN " + typeString + " SHADER:" + gl.getShaderInfoLog(shader));
+        return false;
+    }
+    return shader;
+};
+
+var shader_vertex = compileShader(vShader, gl.VERTEX_SHADER, "VERTEX");
+var shader_fragment = compileShader(fShader, gl.FRAGMENT_SHADER, "FRAGMENT");
+var SHADER_PROGRAM = gl.createProgram();
+gl.attachShader(SHADER_PROGRAM, shader_vertex);
+gl.attachShader(SHADER_PROGRAM, shader_fragment);
+
+gl.linkProgram(SHADER_PROGRAM);
+
+var _position = gl.getAttribLocation(SHADER_PROGRAM, "aVertexPosition");
+gl.enableVertexAttribArray(_position);
+gl.useProgram(SHADER_PROGRAM);
+
+var triangle_vertex = [
+    -1, -1,
+    1, -1, 
+    1, 1
+];
+
+var TRIANGLE_VERTEX = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, TRIANGLE_VERTEX);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangle_vertex), gl.STATIC_DRAW);
+
+var triangle_faces = [0, 1, 2];
+var TRIANGLE_FACES = gl.createBuffer();
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, TRIANGLE_FACES);
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, 
+    new Uint16Array(triangle_faces), gl.STATIC_DRAW);
+
+
+gl.clearColor(0, 0, 0, 0);
+
+var animate = function() {
+gl.clear(gl.COLOR_BUFFER_BIT);
+gl.bindBuffer(gl.ARRAY_BUFFER, TRIANGLE_VERTEX);
+gl.vertexAttribPointer(_position, 2, gl.FLOAT, false, 4*2, 0);
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, TRIANGLE_FACES);
+gl.drawElements(gl.TRIANGLES, 3, gl.UNSIGNED_SHORT, 0);
+gl.flush();
+window.requestAnimationFrame(animate);
+};
+
+animate();
+
+}
+
+window.addEventListener('load', main);
